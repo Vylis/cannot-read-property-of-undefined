20e1e16b25c2d5f5b458bf808a3516a756f84dbb
vylis <vylis@users.noreply.github.com>
2020-11-19 14:42:09 +0100

    admin monster form done

--- front/src/App.js
+++ front/src/App.js
@@ -5,23 +5,25 @@ import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import Map from './components/Map/Map';
 import Footer from './components/Footer/Footer';
+import Admin from './components/Admin/Admin';
 
 import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/map" component={Map} />
-        </Switch>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Header />
+				<NavBar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/map' component={Map} />
+					<Route exact path='/admin' component={Admin} />
+				</Switch>
+				<Footer />
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
