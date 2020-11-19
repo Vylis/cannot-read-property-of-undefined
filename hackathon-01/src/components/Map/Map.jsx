d2a378cdabfe6dc6d86ab1f9846662617121c844
alexpoirrier <alex.poirrier@gmail.com>
2020-11-19 14:51:36 +0100

    DIVISED the map

--- hackathon-01/src/components/Map/Map.jsx
+++ hackathon-01/src/components/Map/Map.jsx
@@ -1,13 +1,35 @@
-import React from 'react'; 
-import map from '../../styles/pictures/map.png'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import map from "../../styles/pictures/map.png";
+import "../../styles/css/Map/Map.css";
+
+const nbFraction = 25;
+const fractionArray = [];
+for (let i = 0; i < nbFraction; i++) {
+  fractionArray.push("");
+}
 
 const Map = () => {
-  return(
-    <section className=" map_container">
+  return (
+    <section className=" map_page_container">
       <h3>Click on the map to see which monsters are in that location :</h3>
-      <img src={map} alt="Ancient Greece" className="map"/>
+      <div className="map_container">
+        <img src={map} alt="Ancient Greece" className="map" />
+        <div className="map_fraction">
+          {fractionArray.map((element, index) => (
+            <Link to="/list" id={index}>
+              <div
+                to="#map_monsters_list"
+                className="fraction"
+                id={index + 1}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Map;
