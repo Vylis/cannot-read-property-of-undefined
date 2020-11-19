8302c9b586ea1e980624149b9c1bf8d3f7d21f8a
vylis <vylis@users.noreply.github.com>
2020-11-19 17:35:48 +0100

    ok

--- front/src/components/List/MonsterDetails.jsx
+++ front/src/components/List/MonsterDetails.jsx
@@ -1,45 +1,39 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-
-<<<<<<< HEAD
-import NavBar from '../NavBar/NavBar';
-import Footer from '../Footer/Footer';
-=======
->>>>>>> 743f26346bb1a03dfcf602b7a30966b9bb15421f
-import "../../styles/css/List/MonsterDetails.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../../styles/css/List/MonsterDetails.css';
 
 const MonsterDetails = ({ match, location }) => {
-  const { id } = match.params;
-  const [monster, setMonster] = useState({});
+	const { id } = match.params;
+	const [monster, setMonster] = useState({});
 
-  useEffect(() => {
-    axios
-      .get(`https://cannotread.herokuapp.com/api/monsters/${id}`)
-      .then((response) => setMonster(response.data[0]));
-  }, [id]);
+	useEffect(() => {
+		axios
+			.get(`https://cannotread.herokuapp.com/api/monsters/${id}`)
+			.then((response) => setMonster(response.data[0]));
+	}, [id]);
 
-  return (
-    <div>
-      <div className="monster_details_container">
-        <Link to={"/MonsterList"}>
-          <button className="monster_details_button">Return to List</button>
-        </Link>
-        <div className="monster_details intel">
-          <img
-            className="monster_details_image"
-            src={monster.image}
-            alt={monster.name}
-          />
-          <h2>{monster.name}</h2>
-          {/* <p>Can be found at {monster.location}</p> */}
-          <p>Danger rating : {monster.danger}/10</p>
-          <p>{monster.description}</p> 
-          <p>Weakness : {monster.weakness}</p> 
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='monster_details_container'>
+				<Link to={'/MonsterList'}>
+					<button className='monster_details_button'>Return to List</button>
+				</Link>
+				<div className='monster_details intel'>
+					<img
+						className='monster_details_image'
+						src={monster.image}
+						alt={monster.name}
+					/>
+					<h2>{monster.name}</h2>
+					{/* <p>Can be found at {monster.location}</p> */}
+					<p>Danger rating : {monster.danger}/10</p>
+					<p>{monster.description}</p>
+					<p>Weakness : {monster.weakness}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MonsterDetails;
--- front/src/components/List/MonsterList.jsx
+++ front/src/components/List/MonsterList.jsx
@@ -1,36 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-<<<<<<< HEAD
-import NavBar from '../../components/NavBar/NavBar';
-import Footer from '../Footer/Footer';
-=======
->>>>>>> 743f26346bb1a03dfcf602b7a30966b9bb15421f
-import MonsterCard from "./MonsterCard";
-import "../../styles/css/List/MonsterList.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MonsterCard from './MonsterCard';
+import '../../styles/css/List/MonsterList.css';
 
 function MonsterList() {
-  const [monsters, setMonsters] = useState([]);
+	const [monsters, setMonsters] = useState([]);
 
-  useEffect(() => {
-    handleAxios();
-  }, []);
+	useEffect(() => {
+		handleAxios();
+	}, []);
 
-  const handleAxios = () => {
-    axios
-      .get(`https://cannotread.herokuapp.com/api/monsters`)
-      .then((response) => setMonsters(response.data));
-  };
+	const handleAxios = () => {
+		axios
+			.get(`https://cannotread.herokuapp.com/api/monsters`)
+			.then((response) => setMonsters(response.data));
+	};
 
-  return (
-    <div>
-      <div className="monster_list_container">
-        {monsters.map((monster) => (
-          <MonsterCard key={monster.name} {...monster} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='monster_list_container'>
+				{monsters.map((monster) => (
+					<MonsterCard key={monster.name} {...monster} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default MonsterList;
