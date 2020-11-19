959df22297dd6c3f891dba6995e1853c7dd7a830
alexpoirrier <alex.poirrier@gmail.com>
2020-11-19 12:00:09 +0100

    CREATE basic Footer

--- hackathon-01/src/components/Home/Home.jsx
+++ hackathon-01/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import NavBar from './NavBar/NavBar'
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   )
 }
