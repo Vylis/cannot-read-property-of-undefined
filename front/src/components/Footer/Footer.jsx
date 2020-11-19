ef9b42eb8700d503a294e383d6d5f96c67c27eca
Robin <torresrobin45@gmail.com>
2020-11-19 16:30:09 +0100

    changed hackathon to front

---
+++ front/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../styles/css/Footer/Footer.css';
+
+const Footer = () => {
+  return(
+    <div className="footer_container">
+      <p>© Copyright 2020 - Developed by CRPOU</p>
+    </div>
+  )
+}
+
+export default Footer;
