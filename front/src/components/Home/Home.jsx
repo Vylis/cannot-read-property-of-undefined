3ea08a2c4ef4050ebae904e04a99cd209c110f1f
vylis <vylis@users.noreply.github.com>
2020-11-20 12:03:49 +0100

    ok

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,40 +1,44 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import MonsterCard from "../List/MonsterCard";
+import MonsterCard from '../List/MonsterCard';
 
-import "../../styles/css/Home/Home.css";
+import '../../styles/css/Home/Home.css';
 
 const Home = () => {
-  const [mostWanted, setMostWanted] = useState([]);
-  const [lastReward, setLastReward] = useState([]);
+	const [mostWanted, setMostWanted] = useState([]);
+	const [lastReward, setLastReward] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://cannotread.herokuapp.com/api/monsters?wanted=1`)
-      .then((response) =>
-        setMostWanted(response.data.filter((monster) => monster.wanted === 1))
-      );
-    axios
-      .get(`https://cannotread.herokuapp.com/api/monsters?wanted=1`)
-      .then((response) => setLastReward(response.data));
-  }, []);
-  return (
-    <section className="home_container">
-      <h2>Most wanted monsters :</h2>
-      <div className="most_wanted_container">
-        {mostWanted.map((monster) => (
-          <MonsterCard {...monster} />
-        ))}
-      </div>
-      <h2>Last rewards :</h2>
-      <div className="last_reward_container">
-        {lastReward.map((monster) => (
-          <MonsterCard {...monster} />
-        ))}
-      </div>
-    </section>
-  );
+	useEffect(() => {
+		axios
+			.get(`https://cannotread.herokuapp.com/api/monsters?wanted=1`)
+			.then((response) =>
+				setMostWanted(response.data.filter((monster) => monster.danger >= 10))
+			);
+		axios
+			.get(`https://cannotread.herokuapp.com/api/monsters?wanted=1`)
+			.then((response) =>
+				setLastReward(
+					response.data.slice(Math.max(response.data.length - 5, 0))
+				)
+			);
+	}, []);
+	return (
+		<section className='home_container'>
+			<h2>Most wanted monsters :</h2>
+			<div className='most_wanted_container'>
+				{mostWanted.map((monster) => (
+					<MonsterCard {...monster} />
+				))}
+			</div>
+			<h2>Last rewards :</h2>
+			<div className='last_reward_container'>
+				{lastReward.map((monster) => (
+					<MonsterCard {...monster} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Home;
