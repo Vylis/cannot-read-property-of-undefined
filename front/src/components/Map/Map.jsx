85047dff37260616a2f088ce65a52dd11f286bd8
Pol <polcahh@gmail.com>
2020-11-20 03:02:02 +0100

    feature/Signal

--- front/src/components/Map/Map.jsx
+++ front/src/components/Map/Map.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import Signal from "./Signal";
+
 import map from "../../styles/pictures/greece_map.png";
 import "../../styles/css/Map/Map.css";
 
@@ -13,50 +15,60 @@ for (let i = 0; i < nbFraction; i++) {
 
 const Map = () => {
   const [mostWanted, setMostWanted] = useState();
+  const [idSignal, setIdSignal] = useState();
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const monsters = await axios.get(`${process.env.REACT_APP_MYTH_API_URL}/api/monsters`);
+      const monsters = await axios.get(
+        `${process.env.REACT_APP_MYTH_API_URL}/api/monsters`
+      );
       const { data } = monsters;
-      const wanted = data.filter(monster => monster.wanted === 1)
-      setMostWanted(wanted)
+      const wanted = data.filter((monster) => monster.wanted === 1);
+      setMostWanted(wanted);
     })();
-  }, []);
+  }, [refresh]);
+
+  const handleRefresh = () => {
+    setRefresh(!refresh);
+  };
 
   const handleFocus = (i) => {
-    console.log(i + 1);
+    setIdSignal(i + 1);
   };
-  
 
   return (
     <section className=" map_page_container">
       <h3>Click on the map to see which monsters are in that location :</h3>
+      <Signal idSignal={idSignal} />
+      <button onClick={() => handleRefresh()}>OK</button>
       <div className="map_container">
         <img src={map} alt="Ancient Greece" className="map" />
         <div className="map_fraction">
-          {mostWanted && fractionArray.map((element, i) => {
-            if (mostWanted.find(wanted => wanted.lastseen == i + 1)) {
-             
-              return   <button
-                type="button"
-                // to="/list"
-                id={i + 1}
-                className="fraction_wanted"
-                onClick={() => handleFocus(i)}
-              />
-              
-            } else {
-              
-              return  <button
-                type="button"
-                // to="/list"
-                id={i + 1}
-                className="fraction"
-                onClick={() => handleFocus(i)}
-              />
-              
-            }
-          })}
+          {mostWanted &&
+            fractionArray.map((element, i) => {
+              if (mostWanted.find((wanted) => wanted.lastseen == i + 1)) {
+                return (
+                  <button
+                    type="button"
+                    // to="/list"
+                    id={i + 1}
+                    className="fraction_wanted"
+                    onClick={() => handleFocus(i)}
+                  />
+                );
+              } else {
+                return (
+                  <button
+                    type="button"
+                    // to="/list"
+                    id={i + 1}
+                    className="fraction"
+                    onClick={() => handleFocus(i)}
+                  />
+                );
+              }
+            })}
         </div>
       </div>
     </section>
@@ -64,13 +76,3 @@ const Map = () => {
 };
 
 export default Map;
-
-// (
-//   <button
-//   type="button"
-//   // to="/list"
-//   id={i + 1}
-//   className="fraction"
-//   onClick={() => handleFocus(i)}
-// />
-// )
